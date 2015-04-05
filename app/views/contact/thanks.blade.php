8d688c90a0c5fae3cf1978a1d4c37297774cd147
manabu bannai <mnbmnb0524@gmail.com>
2015-04-05 21:53:01 +0800

    add

--- app/views/contact/thanks.blade.php
+++ app/views/contact/thanks.blade.php
@@ -6,6 +6,7 @@
 		<div class="col-xs-12">
 			<h2 class="text-center">{{ Input::get('contactName') }}さん、Phiinoをご利用いただきありがとうございました<(_ _)></h2>
 			<p class="text-center">出品者からの連絡をお待ちください</p>
+			<br /><br /><br />
 		</div>
 	</div>
 </div>
